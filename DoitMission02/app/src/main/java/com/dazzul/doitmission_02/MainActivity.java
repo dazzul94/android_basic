d80051d025131851fc312d316c68921a61fc9009
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2020-07-19 17:59:21 +0900

    연습문제 commit
    DoitMisson01
    DoitMisson02

---
+++ DoitMission02/app/src/main/java/com/dazzul/doitmission_02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dazzul.doitmission_02;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
